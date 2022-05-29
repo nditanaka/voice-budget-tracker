d1c2f7cdcfeabc5928071effa0fe46ee9db20f28
nditanaka <tanaching@gmail.com>
2022-05-29 14:55:24 -0400

    add categories for Income and Expenses

--- src/components/Main/Form/Form.js
+++ src/components/Main/Form/Form.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid'
 
 import useStyles from './styles'
 import { BudgetTrackerContext } from '../../../context/context'
-
+import { incomeCategories, expenseCategories } from '../../../constants/categories'
 
 const initialState = {
     amount: '',
@@ -24,8 +24,8 @@ const Form = () => {
         setFormData(initialState)
 
     }
-
-    console.log('formData', formData)
+    
+    const selectedCategories = formData.type === 'Income' ? incomeCategories : expenseCategories
 
   return (
       <Grid container spacing={2}>
@@ -47,8 +47,7 @@ const Form = () => {
               <FormControl fullWidth>
                   <InputLabel>Category</InputLabel>
                   <Select value={formData.category} onChange={(e) => setFormData({...formData, category: e.target.value})}>
-                      <MenuItem value="business">Business</MenuItem>
-                      <MenuItem value="salary">Salary</MenuItem>
+                      {selectedCategories.map((category) => <MenuItem key={category.type}>{category.type}</MenuItem>)}
                   </Select>
               </FormControl>
           </Grid>
