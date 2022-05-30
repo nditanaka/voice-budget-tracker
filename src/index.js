84de8017d83050a43f768464f764803b4c24b1ce
nditanaka <tanaching@gmail.com>
2022-05-30 05:23:01 -0400

    clean up webVitals

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { SpeechProvider } from '@speechly/react-client';
 import App from './App';
 import { Provider } from './context/context';
-// import reportWebVitals from './reportWebVitals';
+
 import './index.css'
 
 ReactDOM.render(
@@ -14,8 +14,3 @@ ReactDOM.render(
     </SpeechProvider>,
     document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
